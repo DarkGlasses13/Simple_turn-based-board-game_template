1936fa1a8b5ff39cb22226b23224dc41cd306e10
Matroskin_Cat <groznov3@gmail.com>
2023-09-19 23:08:23 +0300

    Update ActorsBase.cs

--- Unity.BoardGame/Assets/_Project/Actors Base/ActorsBase.cs
+++ Unity.BoardGame/Assets/_Project/Actors Base/ActorsBase.cs
@@ -7,24 +7,22 @@ namespace Assets._Project.Actors_Base
     {
         private readonly List<A> _actors = new();
 
-        public A GetByID(string id)
+        public A GetByID(string id, bool isUsed = false, bool willWse = true)
         {
             A actor = _actors
-                .Where(actor => actor.IsInUse == false)
+                .Where(actor => actor.IsInUse == isUsed)
                 .FirstOrDefault(actor => actor.Data.ID == id);
 
-            if (actor != null)
-            {
-                CreateByID(id);
-            }
+            actor ??= GetNewByID(id, willWse);
 
             return actor;
         }
 
-        public A GetNewByID(string id)
+        public A GetNewByID(string id, bool willWse = true)
         {
             A created = CreateByID(id);
             _actors.Add(created);
+            created.IsInUse = willWse;
             return created;
         }
 
